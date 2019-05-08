5403020089bf87bb7ba1ce818c8cf111243c8da2
Zoltán Maróti <zoli.maroti@gmail.com>
2019-05-08 14:09:46 +0200

    Sorting fixed

--- app.py
+++ app.py
@@ -1,64 +1,24 @@
 from flask import Flask, render_template, redirect, request
 from datetime import datetime
-import connection
 import data_manager
-import time
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def show_all_questions():
-
-    """
-    order_selection = request.args.get('selection')
-    questions = connection.get_questions_file()
-
-    if order_selection is not None:
-        questions = sorted(questions, key=lambda item: item[order_selection].lower())
-
-    questions = data_manager.convert_numbers_to_int(questions)
-
-    timestamps = data_manager.convert_timestamp(questions)
-    return render_template('list.html', questions=questions, timestamps=timestamps)
-    """
-
-
-    questions = data_manager.get_questions()
-    return render_template('list.html', questions=questions)
-
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/list', methods=['GET', 'POST'])
-def sort_questions():
-    if request.method == 'POST':
-        sorted_questions = data_manager.sort_questions(request.form['selection'], request.form['order'])
-    return render_template('list.html', questions=sorted_questions)
+def show_all_questions():
+    if request.method == 'GET':
+        questions = data_manager.get_questions()
+        return render_template('list.html', questions=questions)
+
+    elif request.method == 'POST':
+        questions = data_manager.sort_questions(request.form['selection'], request.form['order'])
+        return render_template('list.html', questions=questions)
 
 
 @app.route('/question/<question_id>', methods=['GET', 'POST'])
 def view_question(question_id):
-
-    """
-    selected_question = data_manager.get_question_id(connection.get_questions_file(), question_id)
-    answers = connection.get_answers_from_file()
-
-    if request.method == 'POST':
-        new_answer = {
-            'id': data_manager.generate_random(answers),
-            'submission_time': int(time.time()),
-            'vote_number': '1',
-            'question_id': question_id,
-            'message': request.form['message'],
-            'vote': 0
-        }
-        connection.write_answer_to_file(new_answer)
-        return redirect(request.url)
-
-    elif request.method == 'GET':
-        return render_template('question.html', question=selected_question, title='Question', answers=answers, question_id=question_id)
-    """
-
-
-
 
     if request.method == 'GET':
         selected_question = data_manager.get_selected_question(question_id)
@@ -114,32 +74,9 @@ def edit_question(question_id):
     selected_question = data_manager.get_selected_question(question_id)
     if request.method == 'POST':
         data_manager.edit_question(question_id, request.form['title'], request.form['message'])
-        return  redirect('/list')
+        return redirect('/list')
     return render_template('edit-question.html', question=selected_question)
 
-"""
-@app.route('/question/<question_id>/edit', methods=['POST', 'GET'])
-def edit_question(question_id):
-    selected_question = data_manager.get_question_id(connection.get_questions_file(), question_id)
-    return render_template('edit-question.html', question=selected_question, title='Edit')
-
-
-@app.route('/question/<question_id>/delete')
-def delete_question(question_id):
-    questions = connection.get_questions_file()
-    updated_questions = data_manager.delete_question_by_id(question_id, questions)
-    connection.delete_story_from_file(updated_questions)
-    return redirect('/')
-
-
-@app.route('/question/<question_id>/<answer_id>/vote')
-def vote_answer(question_id,answer_id):
-    answers = connection.get_answers_file()
-    for answer in answers:
-        if answer['id'] == answer_id:
-            answer['vote'] = str(int(answer['vote']) + 1)
-    return redirect('/question/<question_id>')
-"""
 
 if __name__ == '__main__':
     app.run(
--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,7 @@
 import connection
 from psycopg2 import sql
 
+
 @connection.connection_handler
 def get_questions(cursor):
     cursor.execute("""
@@ -10,22 +11,12 @@ def get_questions(cursor):
     return questions
 
 
-#@connection.connection_handler
-#def sort_questions(cursor, selection, order):
- #   cursor.execute(sql.SQL("""SELECT * FROM question ORDER BY {} {}""")
-  #                 .format(sql.Identifier(selection), sql.Identifier(order)).as_string(cursor)
-   # questions = cursor.fetchall()
-    #return questions
-
 @connection.connection_handler
 def sort_questions(cursor, selection, order):
-    cursor.execute(sql.SQL("""SELECT * FROM question ORDER BY {selection} {order} ;""")
-                   .format(selection=sql.Identifier(selection), order=sql.Identifier(order)))
+    cursor.execute(sql.SQL("""SELECT * FROM question 
+                              ORDER BY {selection} {order};""").format(selection=sql.SQL(selection), order=sql.SQL(order)))
     questions = cursor.fetchall()
     return questions
-
-
-
 
 
 @connection.connection_handler
@@ -88,25 +79,11 @@ def edit_answer(cursor, id, message):
 
 @connection.connection_handler
 def delete_question_and_answer(cursor, id):
-
-    # TODO: fix bug: 'update or delete on table "question" violates foreign key constraint "question_id" on table "answer"'
-
-    '''
-    cursor.execute("""
-                DELETE FROM answer
-                WHERE id = %(id)s;
-                """,
-               {'id': id})
-    '''
-
     cursor.execute("""
                     DELETE FROM question
                     WHERE id = %(id)s;
                     """,
                    {'id': id})
-
-     # TODO make it work with any table
-
 
 
 @connection.connection_handler
